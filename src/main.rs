5a21752fd1de91483f2fa440df86691d2adf8540
Brett Mayson <brett@mayson.io>
2025-01-05 03:43:58 +0000

    handle existing

--- src/main.rs
+++ src/main.rs
@@ -70,14 +70,6 @@ fn main() {
                 _ => {}
             }
         }
-        if !saw_ebo {
-            for key_dir in ["key", "keys"] {
-                let key_dir = dir.path().join(key_dir);
-                if key_dir.exists() {
-                    std::fs::remove_dir_all(key_dir).expect("can't remove key dir");
-                }
-            }
-        }
         if !saw_pbo {
             continue;
         }
@@ -189,6 +181,12 @@ fn main() {
         ) else {
             continue;
         };
+        for key_dir in ["key", "keys"] {
+            let key_dir = mod_folder.join(key_dir);
+            if key_dir.exists() {
+                std::fs::remove_dir_all(key_dir).expect("can't remove key dir");
+            }
+        }
         private
             .to_public_key()
             .write(
